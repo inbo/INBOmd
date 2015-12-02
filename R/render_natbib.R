6086c8896e6f56a5137843381d8ed641488daa98
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-02 22:58:13 +0100

    render_natbib() gains a display argument

--- R/render_natbib.R
+++ R/render_natbib.R
@@ -1,16 +1,18 @@
 #' Render a Markdown file with natbib bibliography
 #' @export
 #' @importFrom rmarkdown render
-#' @importFrom assertthat assert_that is.string
+#' @importFrom assertthat assert_that is.string is.flag noNA
 #' @param file the name of the Rmd file
 #' @param path the path of the Rmd file
 #' @param enconding the encoding of the Rmd file. Default to 'UTF-8'
 #' @param engine the LaTeX engine the compile the document. Defaults to "xelatex".
+#' @param display open the pdf in a reader. Defaults to TRUE
 render_natbib <- function(
   file,
   path = ".",
   encoding = "UTF-8",
-  engine = c("xelatex", "pdflatex")
+  engine = c("xelatex", "pdflatex"),
+  display = TRUE
 ){
   engine <- match.arg(engine)
   assert_that(is.string(file))
@@ -19,6 +21,8 @@ render_natbib <- function(
   assert_that(file_test("-d", path))
   assert_that(file_test("-f", paste(path, file, sep = "/")))
   assert_that(grepl("\\.[Rr]md$", file))
+  assert_that(is.flag(display))
+  assert_that(noNA(display))
 
   current <- getwd()
   setwd(path)
@@ -32,6 +36,8 @@ render_natbib <- function(
   system(paste("bibtex", output))
   system(paste(engine, output))
   system(paste(engine, output))
-  file.show(paste0(output, ".pdf"))
+  if (display) {
+    file.show(paste0(output, ".pdf"))
+  }
   setwd(current)
 }
