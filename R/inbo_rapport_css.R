360e1ff4c360b5ad38c3441517b56fb5a1b83db3
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-04-28 16:22:10 +0200

    make language specific templates for gitbook and epub reports

--- R/inbo_rapport_css.R
+++ R/inbo_rapport_css.R
@@ -1,16 +1,19 @@
 #' set correct css file and bookdown template
-#' @param format the required output format. Either "html" or "epub"
+#' @param format the required output format. Either "html" or "epub".
+#' @param language the language used in the colophon.
 #' @export
 #' @family utils
-inbo_rapport_css <- function(format = "html") {
-  source_dir <- system.file(
-    "css",
-    package = "INBOmd"
-  )
+inbo_rapport_css <- function(
+  format = c("html", "epub"), language = c("nl", "en")
+) {
+  language <- match.arg(language)
+  format <- match.arg(format)
+  source_dir <- system.file("css", package = "INBOmd")
   file.copy(source_dir, getwd(), recursive = TRUE, overwrite = TRUE)
-  if (format == "epub") {
-    file.path("css", "inbo.epub3")
-  } else {
-    file.path("css", "gitbook.html")
-  }
+  file.path(
+    "css",
+    sprintf(
+      ifelse(format == "epub", "inbo_%s.epub3", "gitbook_%s.html"), language
+    )
+  )
 }
