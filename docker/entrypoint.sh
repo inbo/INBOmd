29313749fe1b18d45a8c77b756d0a43a965596f4
ElsLommelen <els.lommelen@inbo.be>
2021-12-10 16:23:53 +0100

    update website of inbomd_examples in GHA

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -26,4 +26,19 @@ fi
 echo '\nAll Rmarkdown files rendered successfully\n'
 if [ $GITHUB_REPOSITORY = "inbo/inbomd" ] ; then
   cp -R /examples/docs/. $GITHUB_WORKSPACE/docs/.
+else
+  cp -R docs ../docs
+  if [ -z  "$(git branch -r | grep origin/gh-pages)" ]; then
+    git checkout --orphan gh-pages
+    git rm -rf --quiet .
+    git commit --allow-empty -m "Initializing gh-pages branch"
+  else
+    git checkout -b gh-pages
+    git rm -rf --quiet .
+    rm -R *
+  fi
+  cp -R ../docs/. .
+  git add --all
+  git commit --amend -m "Automated update of gh-pages with inbomd_examples"
+  git push --force --set-upstream origin gh-pages
 fi
