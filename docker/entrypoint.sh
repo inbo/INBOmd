b5a53beabbfb5c6cfef810a05411e6c7cb24eaa4
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-12-15 13:07:30 +0100

    update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
 
-echo 'repository:     ' $GITHUB_REPOSITORY
-echo 'ref:            ' $GITHUB_REF
-echo 'head_ref:       ' $GITHUB_HEAD_REF
-echo 'Event:          ' $GITHUB_EVENT_NAME
-
 export HOME=/root
 
 cd $GITHUB_WORKSPACE
@@ -26,19 +21,17 @@ fi
 echo '\nAll Rmarkdown files rendered successfully\n'
 if [ $GITHUB_REPOSITORY = "inbo/inbomd" ] ; then
   cp -R /examples/docs/. $GITHUB_WORKSPACE/docs/.
-else
-  cp -R docs ../docs
-  if [ -z  "$(git branch -r | grep origin/gh-pages)" ]; then
-    git checkout --orphan gh-pages
-    git rm -rf --quiet .
-    git commit --allow-empty -m "Initializing gh-pages branch"
-  else
-    git checkout -b gh-pages
-    git rm -rf --quiet .
-    rm -R *
-  fi
-  cp -R ../docs/. .
+fi
+
+if [ $GITHUB_EVENT_NAME = "push" ] ; then
+  cd /
+  git clone --depth 1 -b gh-pages https://$INPUT_TOKEN@github.com/inbo/inbomd_examples
+  cd /inbomd_examples
+  git rm -rf --quiet .
+  cp -R $GITHUB_WORKSPACE/docs/. /inbomd_examples/.
   git add --all
+  git config --global user.email "bwk@inbo.be"
+  git config --global user.name "INBO BMK"
   git commit --amend -m "Automated update of gh-pages with inbomd_examples"
   git push --force --set-upstream origin gh-pages
 fi
