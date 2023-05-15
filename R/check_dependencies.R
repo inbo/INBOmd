e59aea050e4b240135596bc12d086cd7f1848e55
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-05-15 17:27:35 +0200

    check_dependencies() handles tinytex issues

--- R/check_dependencies.R
+++ R/check_dependencies.R
@@ -1,9 +1,41 @@
 #' Check if the required dependencies of INBOmd are installed
 #' Missing dependencies will be installed automatically
 #' @export
+#' @importFrom tinytex check_installed install_tinytex is_tinytex tlmgr_conf
 #' @importFrom utils install.packages
 #' @family utils
 check_dependencies <- function() {
+  if (!is_tinytex()) {
+    install_tinytex(
+      extra_packages = c(
+        "babel-dutch", "babel-english", "babel-french", "beamer",
+        "beamerswitch", "booktabs", "carlisle", "colortbl", "datetime", "dvips",
+        "emptypage", "environ", "epstopdf", "eso-pic", "eurosym", "extsizes",
+        "fancyhdr", "fancyvrb", "fmtcount", "float", "fontspec", "footmisc",
+        "framed", "helvetic", "hyphen-dutch", "hyphen-french", "inconsolata",
+        "lastpage", "lipsum", "makecell", "marginnote", "mdframed", "ms",
+        "multirow", "parskip", "pdflscape", "pdfpages", "pdftexcmds",
+        "placeins", "needspace", "tabu", "tex", "textpos", "threeparttable",
+        "threeparttablex", "titlesec", "times", "tocloft", "translator",
+        "trimspaces", "ulem", "upquote", "wrapfig", "xcolor", "xstring", "zref"
+      )
+    )
+  }
+  stopifnot("unable to install TinyTeX" = is_tinytex())
+  if (
+    !any(grepl(
+      system.file("local_tex", package = "INBOmd"),
+      tlmgr_conf(c("auxtrees", "show"), .quiet = TRUE, stdout = TRUE)
+    ))
+  ) {
+    tlmgr_conf(
+      c(
+        "auxtrees", "add", system.file("local_tex", package = "INBOmd"),
+        .quiet = TRUE
+      )
+    )
+  }
+
   dependencies <- c("bookdown", "webshot")
   available <- basename(find.package(dependencies, quiet = TRUE))
   to_install <- dependencies[!dependencies %in% available]
@@ -23,6 +55,9 @@ check_dependencies <- function() {
   }
   if (is.null(find_phantom())) {
     webshot::install_phantomjs()
+  }
+  if (.Platform$OS.type == "unix") {
+    Sys.setenv(OPENSSL_CONF = "/dev/null") # nolint: absolute_path_linter.
   }
   return(TRUE)
 }
