fc4ac9381027eda08434a1fe7638690ae023c592
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-04-17 14:58:48 +0200

    ✨ Add_author() handles flandersqmd quarto documents

--- R/add_author.R
+++ R/add_author.R
@@ -10,6 +10,18 @@
 add_author <- function(path = ".") {
   assert_that(is.string(path), noNA(path))
   assert_that(is_dir(path), msg = "`path` is not an existing directory")
+  if (is_file(path(path, "_bookdown.yml"))) {
+    return(add_author_bookdown(path = path))
+  }
+  if (is_file(path(path, "_quarto.yml"))) {
+    return(add_author_quarto(path = path))
+  }
+  stop("Neither `_bookdown.yml` nor `_quarto.yml` found in `path`")
+}
+
+#' @importFrom assertthat assert_that is.string noNA
+#' @importFrom fs is_dir is_file path
+add_author_bookdown <- function(path) {
   assert_that(
     is_file(path(path, "_bookdown.yml")),
     msg = "No `_bookdown.yml` found in `path`"
@@ -41,5 +53,55 @@ add_author <- function(path = ".") {
   head(index, insert) |>
     c(extra, tail(index, -insert)) |>
     writeLines(path(path, "index.Rmd"))
+  return(invisible(NULL))
+}
+
+#' @importFrom assertthat assert_that is.string noNA
+#' @importFrom fs is_dir is_file path
+#' @importFrom yaml read_yaml write_yaml
+add_author_quarto <- function(path) {
+  assert_that(
+    is_file(path(path, "_quarto.yml")),
+    msg = "No `_quarto.yml` found in `path`"
+  )
+  path(path, "_quarto.yml") |>
+    read_yaml() -> yaml
+  stopifnot(
+    "No `lang:` entry found in `_quarto.yml`" = has_name(yaml, "lang"),
+    "No `flandersqmd:` entry found in `_quarto.yml`" =
+      has_name(yaml, "flandersqmd"),
+    "No `author:` entry found in the `flandersqmd` section of `_quarto.yml`" =
+      has_name(yaml$flandersqmd, "author")
+  )
+  lang <- yaml$lang
+  extra <- check_author(lang = lang)
+  yaml$flandersqmd$author <- c(
+    yaml$flandersqmd$author,
+    list(
+      list(
+        name = list(given = extra$given, family = extra$family),
+        email = extra$email, orcid = extra$orcid,
+        affiliation = list(extra$affiliation)
+      )
+    )
+  )
+  yaml$flandersqmd$author <- lapply(
+    yaml$flandersqmd$author,
+    FUN = function(x) {
+      if (!is.list(x$affiliation)) {
+        x$affiliation <- as.list(x$affiliation)
+      }
+      return(x)
+    }
+  )
+  write_yaml(
+    yaml, file = path(path, "_quarto.yml"),
+    handlers = c(
+      "logical" = function(x) {
+        attr(x, "class") <- "verbatim"
+        ifelse(x, "true", "false")
+      }
+    )
+  )
   return(invisible(NULL))
 }
